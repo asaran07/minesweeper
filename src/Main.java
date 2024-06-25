10f5a62687024a94ee75772780c978da570f6488
asaran07 <saran.arsh07@icloud.com>
2024-06-24 21:37:30 -0700

    Added functionality to update new positions around the mines.

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,77 @@ public class Main {
     public static void main(String[] args) {
         String sampleString = """
                 ..*..
-                ...*.
+                ....*
                 .....
                 .*...
-                .....
+                *....
                 """;
         int length = 5;
         int width = 5;
+
         char[][] gameBoard = new char[length][width];
         Scanner scanner = new Scanner(sampleString);
-        for (int i = 0; i < length; i++) {
+
+        fillNewGameBoard(gameBoard, length, width, scanner);
+
+        System.out.println("Original game board:");
+        printBoard(gameBoard);
+
+        updateGameBoard(gameBoard);
+
+        System.out.println("Updated game board:");
+        printBoard(gameBoard);
+    }
+
+    private static void fillNewGameBoard(char[][] gameBoard, int length, int width, Scanner scanner) {
+        for (int y = 0; y < length; y++) {
             String line = scanner.nextLine();
-            for (int j = 0; j < width; j++) {
-                if (line.charAt(j) == '*') {
-                    gameBoard[i][j] = '*';
-                }
-                else if (line.charAt(j) == '.') {
-                    gameBoard[i][j] = '.';
+            for (int x = 0; x < width; x++) {
+                if (line.charAt(x) == '*') {
+                    gameBoard[y][x] = '*';
+                } else if (line.charAt(x) == '.') {
+                    gameBoard[y][x] = '0';
                 }
             }
         }
+    }
 
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[i].length; j++) {
-                System.out.print(gameBoard[i][j] + " ");
+    private static void updateGameBoard(char[][] gameBoard) {
+        for (int y = 0; y < gameBoard.length; y++) {
+            for (int x = 0; x < gameBoard[y].length; x++) {
+                if (gameBoard[y][x] == '*') {
+                    updateNewPosition(gameBoard, y, x);
+                }
+            }
+        }
+    }
+
+    private static void printBoard(char[][] gameBoard) {
+        for (char[] chars : gameBoard) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
             }
             System.out.println();
         }
+    }
+
+    private static void updateNewPosition(char[][] gameBoard, int y, int x) {
+        int[] rowOffsets = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] colOffsets = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for (int i = 0; i < rowOffsets.length; i++) {
+            int newPositionY = y + rowOffsets[i];
+            int newPositionX = x + colOffsets[i];
+            if (inBounds(gameBoard, newPositionY, newPositionX) && isValidInt(gameBoard[newPositionY][newPositionX])) {
+                gameBoard[newPositionY][newPositionX] += 1;
+            }
+        }
+    }
+
+    private static boolean inBounds(char[][] board, int y, int x) {
+        return y >= 0 && y < board.length && x >= 0 && x < board[0].length;
+    }
+
+    private static boolean isValidInt(char theValue) {
+        return theValue >= '0' && theValue <= '8';
     }
 }
